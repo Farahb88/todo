96b2def9cb3048c62d62c11b52c3c9207b3dfd5d
Farah Albahra <farahb88@gmail.com>
2022-11-18 10:06:27 +0300

    todo

--- todolistfarah.js
+++ todolistfarah.js
@@ -1,95 +1,98 @@
-var mylist = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < mylist.length; i++) {
-  var span = document.createElement("span");
-  var txt = document.createTextNode("");
-  span.className = "close";
-  span.appendChild(txt);
-  mylist[i].appendChild(span);
-}
+const input = document.getElementById("todo-input");
+const btn = document.getElementById("todo-btn");
+let todoesArr = [];
+const list = document.querySelector("ul");
 
-function newElement() {
-  var x = Math.floor(Math.random() * 256);
-  var y = Math.floor(Math.random() * 256);
-  var z = Math.floor(Math.random() * 256);
-  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-  var li = document.createElement("li");
-  li.style.background = bgColor;
-
-  var inputValue = document.getElementById("search").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === "") {
-    alert("You must write something!!!");
-  } else {
-    document.getElementById("mytodo").appendChild(li);
-  }
-
-  document.getElementById("search").value = "";
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  var close = document.getElementsByClassName("close");
-  var i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
-
-  var list = document.querySelector("ul");
-  list.addEventListener(
-    "click",
-    function (evt) {
-      if (evt.target.tagName === "li") {
-        evt.target.classList.toggle("checked");
-      }
-    },
-    false
-  );
-}
-
-var mylist = [];
-
-if (localStorage.getItem("mylist")) {
-  mylist = localStorage.getItem("mylist");
-} else {
-() => {
-    localStorage.setItem("mylist, mylist");
+const randomColor = () => {
+  return {
+    red: Math.random() * 256,
+    green: Math.random() * 256,
+    blue: Math.random() * 256,
   };
-}
+};
 
 
-// let idealCar = {
-//   name: 'Mustang',
-//   color: 'Red',
-//   weightInPounds: 3600,
-//   myCar: function () {
-//       console.log('My ideal car is a ' + this.name + ' with a color of ' + this.color + ', but I do not want it to be over ' +
-//           this.weightInPounds + ' pounds.');
-//   }
-// };
-
-// idealCar.myCar();
-
-// const doubleAge = (age) => {
-//   return  age * 2;}
-//   console.log(doubleAge)
- 
-//   const defaultParam = (a, b=10) => {
-//     return a * b; 
-   
-//   }
-//   console.log(defaultParam)
-
-
-
-//   const addName = (firstName,lastName ) => {
-//    {return firstName+" "+lastName ;
-// }
-//   }
+btn.addEventListener("click", () => {
+  const inputText = input.value.trim();
+  if (inputText != "") {
+    if(inputText.length < 20){
+        const newTodoObj = {
+            id: todoesArr.length,
+            content: inputText,
+            color: randomColor(),
+            isDone: false,
+          };
+          todoesArr = [...todoesArr, newTodoObj] 
+          //todoesArr.push(newTodoObj)
+          display(todoesArr);
+    }else{
+        window.alert("The todo task should be less than 20 characters!")    
+    }
+  } else {
+    window.alert("please Enter the todo task!")
+  }
+  input.value = "";
   
+});
+
+const setStorage = (todoes) => {
+  localStorage.setItem("todoes", JSON.stringify(todoes));
+};
+
+const isLight = (color) =>
+  (color.red * 299 + color.green * 587 + color.blue * 114) / 1000 > 155;
+
+const display = (arr) => {
+  list.innerHTML = "";
+  const newRandomColor = randomColor();
+  arr.map((item) => {
+    list.innerHTML += `
+        <li id="${item.id}" style="background-color: rgb(${item.color.red},${
+      item.color.green
+    }, ${item.color.blue})">
+        <p style="color: ${
+          isLight(item.color) ? "black" : "white"
+        }; text-decoration: ${item.isDone ? "line-through" : "none"}">${
+      item.content
+    }</p>
+        <div style="color: ${isLight(item.color) ? "black" : "white"} ">
+          <span class="material-symbols-outlined" onclick="checkTodo(${
+            item.id
+          })">
+            done
+          </span>
+          <span class="material-symbols-outlined" onclick="deleteTodo(${item.id})">
+            delete
+          </span>
+        </div>
+      </li>
+        `;
+  });
+  setStorage(todoesArr);
+};
+
+const getStorage = () => {
+  if (localStorage.getItem("todoes") != null)
+    todoesArr = JSON.parse(localStorage.getItem("todoes"));
+  display(todoesArr);
+};
+
+getStorage();
+
+const deleteTodo = (id) => {
+  const index = todoesArr.findIndex((item) => item.id == id);
+  todoesArr.splice(index, 1);
+  display(todoesArr);
+};
+
+const checkTodo = (id) => {
+  const index = todoesArr.findIndex((item) => item.id == id);
+  todoesArr[index].isDone = !todoesArr[index].isDone;
+  display(todoesArr);
+};
+
+const refresh = () => {
+    localStorage.clear();
+    todoesArr = []
+    display(todoesArr);
+}
